a7b96071a756749f514eea4e0e95f4fd501fe1a0
Daniel Leão <daniellima.rl@gmail.com>
2020-06-27 03:55:04 -0300

    Adicionando Identity e JWT

---
+++ ProAgil.Domain/Identity/User.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace ProAgil.Domain.Identity
+{
+    public class User : IdentityUser<int>
+    {
+        public string FullName { get; set; }
+        public List<UserRole> UserRoles { get; set; }
+
+    }
+}
